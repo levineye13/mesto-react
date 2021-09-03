cc338ed8be8706a7df1c114b52ca62b139210df6
Oleg Zharov <levineye@gmail.com>
2021-09-03 21:17:42 +0300

    feat: переписать на tsx футер

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { FC, ReactElement } from 'react';
 
-const Footer = function () {
+const Footer: FC = (): ReactElement => {
   return (
     <footer className="footer page__footer">
       <p className="footer__copyright">&copy; 2020 Mesto Russia</p>
