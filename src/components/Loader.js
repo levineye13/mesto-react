f56949b8a5bc26970440a35099610048ec7ccc6c
Oleg Zharov <levineye@gmail.com>
2021-09-03 21:17:59 +0300

    feat: переписать на tsx лоадер загрузки

--- src/components/Loader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loader = React.memo(function () {
-  return <div className="animation animation_type_loader"></div>;
-});
-
-export default Loader;
---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+import React, { FC, ReactElement } from 'react';
+
+const Loader: FC = (): ReactElement => (
+  <div className="animation animation_type_loader"></div>
+);
+
+export default Loader;
